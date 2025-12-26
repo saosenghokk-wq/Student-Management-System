561997409031e247aa600410d921370382355664
SENG HOK <saosenghokk@gmail.com>
2025-12-26 11:07:45 +0700

    Add semesters 1-8 to subject enrollment form

--- frontend/src/pages/SubjectEnrollment.jsx
+++ frontend/src/pages/SubjectEnrollment.jsx
@@ -491,6 +491,12 @@ export default function SubjectEnrollment() {
                       <select name="semester" value={form.semester} onChange={handleChange} required>
                         <option value="1">Semester 1</option>
                         <option value="2">Semester 2</option>
+                        <option value="3">Semester 3</option>
+                        <option value="4">Semester 4</option>
+                        <option value="5">Semester 5</option>
+                        <option value="6">Semester 6</option>
+                        <option value="7">Semester 7</option>
+                        <option value="8">Semester 8</option>
                       </select>
                     </div>
 
